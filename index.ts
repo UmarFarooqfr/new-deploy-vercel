a75dfc8382a3c7d37bd7a255b2e2142f886e4c90
Umar Farooq <131238639+UmarFarooqfr@users.noreply.github.com>
2024-05-02 20:28:41 +0500

    Add files via upload
    agaian1111111

--- index.ts
+++ index.ts
@@ -1,51 +1,16 @@
-import express from "express";
-import mongoose from "mongoose";
-import http from "http";
-import { routes } from "./route";
-
-const bodyParser = require("body-parser");
-const cors = require("cors");
+import express, { Request, Response } from "express";
 
 const app = express();
-const server = http.createServer(app);
+app.use(express.json());
 
-app.use(express.json({ limit: "50mb" }));
-app.use(express.urlencoded({ limit: "50mb", extended: true }));
-app.use(bodyParser.json({ limit: "50mb" }));
-app.use(cors({ origin: "*" }));
-
-app.use((req, res, next) => {
-  res.setHeader("Content-Type", "application/json");
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET,PUT, POST, PATCH, DELETE, OPTIONS"
-  );
-  next();
+// get post routes
+app.get("/post", (req: Request, res:Response) => {
+  res.status(200).json({ message: "post routes" });
 });
 
-const PORT = 3000;
-export const TOKEN_KEY = "11223344";
-routes(app);
-const URL =
-  "mongodb+srv://wwwburjsoft:gBLTbWrZsDYS9kR5@cluster0.k2bwmju.mongodb.net/";
-
-server.listen(PORT, () => {
-  console.log(`Express server listening ${PORT}`);
-  app.get("/", function (req, res) {
-    res.send("**** Hello World! ****");
-  });
+// root routes
+app.get("/", (req: Request, res:Response) => {
+  res.status(200).json({ message: "Hello World" });
 });
-
-async function connection() {
-  await mongoose.connect(URL).then(() => {
-    console.log("connection success");
-  });
-}
-connection();
-
-// export default app;
+app.listen(5000,()=>console.log('server Running'))
+export default app;
